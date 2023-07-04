5d1eceb42890967c6e15d68f5927b0773fe84288
Tyler <tfritzy@gmail.com>
2023-07-04 15:04:41 -0700

    Working on UI styling

--- src/GameMenu/GameMenu.tsx
+++ src/GameMenu/GameMenu.tsx
@@ -50,31 +50,53 @@ export const GameMenu = (props: GameMenuProps) => {
   };
 
   return (
-    <div className="grid place-content-center h-screen h-48 text-white">
-      <div className="flex flex-col space-y-12">
-        <button onClick={handleCreateGame}>Create Game</button>
+    <div className="grid place-content-center h-screen text-white">
+      <div className="rounded-md border-2 border-slate-600 bg-slate-700 p-4 w-80">
+        <div className="text-3xl text-center mb-4">Aces</div>
 
-        <div className="flex flex-col">
-          <div className="text-black">
+        <div className="flex flex-col space-y-8 divide-y divide-slate-500">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-white">
+              Display name
+            </label>
             <input
               type="text"
-              value={joinGameInput}
-              placeholder="AIE-JCS"
-              onChange={(e) => setJoinGameInput(e.target.value)}
-            />
-          </div>
-
-          <button onClick={handleJoinGame}>Join Game</button>
-        </div>
-
-        <div>
-          <div>Display name</div>
-          <div className="text-black">
-            <input
-              type="text"
+              id="display_name"
+              className="border text-sm rounded-lg focus:ring-emerald block w-full p-2 bg-slate-600 border-slate-400 placeholder-slate-400 text-white"
               value={props.displayName}
               onChange={(e) => props.setDisplayName(e.target.value)}
             />
+          </div>
+
+          <div className="pt-8 flex flex-col space-y-3">
+            <div className="flex flex-row space-x-2">
+              <div className="grow">
+                <input
+                  type="text"
+                  id="game"
+                  className="border text-sm rounded-lg focus:ring-emerald block w-full h-full p-2 bg-slate-600 border-slate-400 placeholder-slate-400 text-white"
+                  value={joinGameInput}
+                  placeholder="AIE-JCS"
+                  onChange={(e) => setJoinGameInput(e.target.value)}
+                />
+              </div>
+
+              <button
+                className="flex-none rounded-md bg-emerald-500 px-4 p-2 text-slate-50 font-semibold"
+                onClick={handleJoinGame}
+              >
+                Join
+              </button>
+            </div>
+
+            <div className="text-slate-400 text-center">— or —</div>
+
+            <button
+              className="rounded-md bg-emerald-500 p-2 text-slate-50 font-semibold"
+              onClick={handleCreateGame}
+            >
+              Create Game
+            </button>
           </div>
         </div>
       </div>
