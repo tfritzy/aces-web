bfe04115a11b9e139d2dcdef08abd2f3d1b3b6ee
Tyler <tfritzy@gmail.com>
2023-07-05 17:15:19 -0700

    Adding copy box

--- src/components/CopyBox.tsx
+++ src/components/CopyBox.tsx
@@ -5,14 +5,51 @@ type CopyBoxProps = {
 };
 
 export const CopyBox = (props: CopyBoxProps) => {
+  const [copied, setCopied] = React.useState(false);
+  const [hovered, setHovered] = React.useState(false);
+
+  const copy = () => {
+    navigator.clipboard.writeText(props.text);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  };
+
   return (
-    <div className="flex items-center cursor-pointer space-x-2 border border-gray-500 bg-gray-600 max-w-sm font-mono text-lg py-1 px-2 rounded-md text-white">
-      <div className="flex gap-1">
-        <span>{props.text}</span>
+    <div className="relative">
+      <div
+        className="border border-gray-500 bg-gray-600 max-w-sm font-mono text-lg py-1 px-2 rounded-md text-white cursor-pointer"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onClick={copy}
+      >
+        <div className="flex flex-row items-center">
+          <div className="grow">
+            <span>{props.text}</span>
+          </div>
+          <span className="text-gray-500 w-5 h-5 hover:text-gray-400 duration-200">
+            <img src="Icons/clipboard.png" alt="Copy" />
+          </span>
+        </div>
       </div>
-      <span className="flex text-gray-500 w-5 h-5 hover:text-gray-400 duration-200">
-        <img src="Icons/clipboard.png" alt="Copy" />
-      </span>
+      {hovered && (
+        <div className="flex flex-row items-center space-x-1 absolute -top-9 right-0 bg-gray-800 rounded-md text-sm px-2 py-1">
+          {copied ? (
+            <img
+              src="Icons/check.svg"
+              alt="Check"
+              style={{ width: 18, height: 20 }}
+            />
+          ) : (
+            <img
+              src="Icons/copy.svg"
+              alt="Check"
+              style={{ width: 20, height: 20 }}
+            />
+          )}
+
+          <span>{copied ? "Copied" : "Click to copy"}</span>
+        </div>
+      )}
     </div>
   );
 };
