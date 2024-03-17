f2f80a8b61e5a9457fa9f890c85d8fe2e9637c15
Tyler <tfritzy@gmail.com>
2024-03-17 10:57:06 -0700

    Toasts?

--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -40,7 +40,7 @@ export const Toasts = (props: ToastsProps) => {
   return (
     <div className="fixed left-0 top-0 w-screen full-height min-w-screen pointer-events-none pt-2">
       <div
-        className="flex flex-col w-screen items-center space-y-2"
+        className="flex flex-col min-w-screen items-center space-y-2"
         ref={animationParent}
       >
         {props.toasts.map((item) => (
