40c96e7af48ec0195f6a70513aeded8b8265b33e
Tyler <tfritzy@gmail.com>
2024-03-17 09:43:00 -0700

    improving dvh fallback

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,17 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+// Calculate the viewport height in a 'dynamic' way and set a CSS variable with the value
+function setVHVariable() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+// Initial setup
+setVHVariable();
+// Make sure to re-calculate on resize
+window.addEventListener("resize", setVHVariable);
+
 root.render(
   <Provider store={store}>
     <App />
